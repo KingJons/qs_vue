7a1fe418e32b65c9c060e8a49c0ea8ef87b6e4fb
caicong <1161403069@qq.com>
2018-01-13 19:33:42 +0800

    element-ui源码分析

--- router-post/src/App.vue
+++ router-post/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <el-badge class="item" :value="1" :max="9">
+      <el-button size="medium" @click="addComment" nativeType="submit">评论</el-button>
+    </el-badge>
+    <el-badge class="item" :value="10" :max="9" isDot>
+      <el-button type="primary" size="mini" :disabled="saveDisabled" @click="doSave">发布</el-button>
+    </el-badge>
+    <el-badge class="item" :value="10" :max="9">
+      <el-button type="text" size="small">前往</el-button>
+    </el-badge>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import ElBadge from '@/components/ElBadge'
+import ElButton from '@/components/ElButton'
 export default {
   name: 'app',
+  data(){
+    return {
+      saveDisabled: false
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.saveDisabled = true
+    },5000)
+  },
   components: {
-    HelloWorld
+    ElButton,
+    ElBadge
+  },
+  methods: {
+    addComment() {
+      console.log('添加评论');
+    },
+    doSave() {
+      console.log('发布');
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ router-post/src/components/ElButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <button class="el-button" :class="[type? 'el-button--' + type : '', 
+    buttonSize? 'el-button--' + buttonSize : '',{
+        'is-disabled': disabled
+    }]" 
+    @click="handleClick" :type="nativeType" :disabled="disabled">
+        <span v-if="$slots.default"><slot></slot></span>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'default'
+        },
+        size: String,
+        nativeType: {
+            type: String,
+            default: 'default'
+        },
+        disabled: Boolean
+    },
+    computed: {
+        buttonSize() {
+            return this.size
+        }
+    },
+    methods: {
+        handleClick(evt) {
+            this.$emit('click', evt);
+        }
+    }
+        
+    
+}
+</script>
+
+<style>
+
+</style>
